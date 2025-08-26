8fc319538d89d20d4ce1dda0b4bcd3f9ef5076f8
Sakshi0522 <sakshisaha.17@gmail.com>
2025-08-26 12:45:09 +0530

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -37,7 +37,7 @@ export default function Auth() {
           formData.password,
           formData.firstName,
           formData.lastName,
-          formData.phone,
+          formData.phone
         );
         if (!error) {
           navigate('/');
